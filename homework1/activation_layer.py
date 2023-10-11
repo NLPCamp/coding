a289e40d5e06d9212e62af82fb3b233399e81c19
BeckyHan2023 <147653040+BeckyHan2023@users.noreply.github.com>
2023-10-11 13:49:15 -0500

    Update activation_layer.py

--- homework1/activation_layer.py
+++ homework1/activation_layer.py
@@ -10,10 +10,13 @@ class ActivationLayer(Layer):
     def forward_propagation(self, input_data):
         self.input = input_data
         #TODO: apply activation function on the input and return
+        self.output = self.activation(self.input)
+        return self.output
 
 
     # Returns input_error=dE/dX for a given output_error=dE/dY.
     # learning_rate is not used because there is no "learnable" parameters.
     def backward_propagation(self, output_error, learning_rate):
         #TODO: return input_error
+        return self.activation_prime(self.input) * output_error
 
