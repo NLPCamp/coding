4ed67f924d7c24937b55281123f504198f6de0dd
BeckyHan2023 <147653040+BeckyHan2023@users.noreply.github.com>
2023-10-11 13:50:19 -0500

    Update activations.py

--- homework1/activations.py
+++ homework1/activations.py
@@ -3,8 +3,8 @@ import numpy as np
 # activation function and its derivative
 def tanh(x):
     #TODO: return tanh
-
+    return np.tanh(x)
 
 def tanh_prime(x):
     #TODO: return tanh derivative
-
+    return 1 - np.tanh(x) ** 2
