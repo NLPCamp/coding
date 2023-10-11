8402afa4b3bc4de26a6eeb1e7f8f5ff30e7a4cc2
BeckyHan2023 <147653040+BeckyHan2023@users.noreply.github.com>
2023-10-11 13:54:05 -0500

    Update losses.py

--- homework1/losses.py
+++ homework1/losses.py
@@ -3,6 +3,8 @@ import numpy as np
 # loss function and its derivative
 def mse(y_true, y_pred):
     #TODO: return mse
-
+    return np.mean((y_true - y_pred) ** 2)
+    
 def mse_prime(y_true, y_pred):
     #TODO: return mse derivative
+    return 2 * (y_pred - y_true) / len(y_true)
